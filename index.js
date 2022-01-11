d3afb1a760bdd29c624a4b3b2e841d308ee35e0a
Mohamed Mohamud <77820308+mozini206@users.noreply.github.com>
2022-01-10 18:10:49 -0800

    PseudoCode

---
+++ index.js
@@ -0,0 +1,22 @@
+// Actions need
+
+// Ask them for manager info
+
+    //Prompt the user for the data
+        // THEN create and store object for the manager
+        // THEN ask for what they would like to do next
+
+// Ask them for engineer info
+          //Prompt the user for the data
+         // THEN create and store object for the manager
+        // THEN ask for what they would like to do next
+      
+
+// Ask what they would like to do next
+    // Add Engineer, Add Intern, Be Done
+        // IF 'Add Engineer' -> Ask them for engineer info
+        // IF 'Add Intern' -> Ask them for Intern info
+        // IF 'Be Done' => build an html page
+
+
+// Use all of the collected employee employee data to build an html page
