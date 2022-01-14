059ad966cae11d0c920bdaaa55faffaaaf8d76b4
Mohamed Mohamud <momo206@uw.edu>
2022-01-13 18:04:09 -0800

    employee.test complete

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,43 +2,60 @@ const Employee = require('../lib/Employee')
 
 describe("Employee", () => {
 
-    // Methods
+   
+    const employee = new Employee('Mohamed', 21, "momo206@uw.edu");
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.getId).toEqual(expect.any(Number));
+    expect(employee.getEmail).toEqual(expect.any(String));
+
+
+
+
 
     describe("Intialization", () =>{
         
         // Testing of object properties after construction a new instance
         it ("should build with prop x when given y", () => {
-            
+
         })
 
     })
 
     describe("getName", () => {
 
-        // Arrange
-        // Create a new employee object
 
-        //Act
-        // Call the getName method and store the result
-
-        //Assert
-        //The result is equal to an expected value
+        const employee = new Employee('Mohamed', 17, "momo206@uw.edu");
+        
+        expect(employee.getName()).toEqual(expect.any(String));
 
     
 
     });
 
 
-    describe("getId", () => [
+    describe("getId", () => {
+        const employee = new Employee('Mohamed', 21, "momo206@uw.edu");
 
-    ]);
+        expect(employee.getId()).toEqual(expect.any(Number));
 
-    describe("geEmail", () => [
 
-    ]);
+    });
 
-    describe("geRole", () => [
+    describe("geEmail", () => {
+        const employee = new Employee('Mohamed', 21, "momo206@uw.edu");
+        expect(employee.getEmail()).toEqual(expect.any(String));
 
-    ]);
+
+    });
+
+    describe("geRole", () => {
+        const employee = new Employee('Mohamed', 21, "momo206@uw.edu");
+        expect(employee.getRole()).toEqual('Employee');
+
+
+
+
+    });
 
 });
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+const inquirer = require('inquirer');
 const Manager = require('./lib/Manager')
 const employees = [];
 
 // Actions need
 
 // Ask them for manager info
-
+function askManagerInfo(){
     //Prompt the user for the data
-        // THEN create and store object for the manager
-        employees.push (new Manager())// pass in data from inquirer)
-        // THEN ask for what they would like to do next
+    inquirer
+        .prompt('questions')
+        .then((answers) => {
+
+            employees.push (new Manager())// pass in data from inquirer)
+
+        })
+         // THEN ask for what they would like to do next
+            askForNextAction()
+
+        
+
+
+}
+
+async function askForManagerInfo(){
+    //Prompt the user for the data
+    const answers = await inquirer.prompt('questions')
+
+    // THEN create and store object for the manager
+    employees.push (new Manager())// pass in data from inquirer)
+
+    // THEN ask for what they would like to do next
+    askForNextAction()
+
+
+}
+       
 
 // Ask them for engineer info
           //Prompt the user for the data
